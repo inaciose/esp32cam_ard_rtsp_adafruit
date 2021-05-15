e2a9fd2de33586daee50e19df94e4e933606c7a8
inaciose <inaciose@gmail.com>
2021-05-15 23:05:56 +0100

    iniital commit

---
+++ include/wifikeys.h
@@ -0,0 +1,3 @@
+// wifi authentication
+const char *ssid =     "vfxira_EXT";
+const char *password = "0101010101";
---
+++ src/main.cpp
@@ -0,0 +1,318 @@
+#include "OV2640.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+
+#include "SimStreamer.h"
+#include "OV2640Streamer.h"
+#include "CRtspSession.h"
+
+// for led blink
+//#define LED_BUILTIN 33
+#ifdef LED_BUILTIN
+bool ledon = false;
+#endif
+
+#define ENABLE_OLED //if want use oled ,turn on thi macro
+// #define SOFTAP_MODE // If you want to run our own softap turn this on
+#define ENABLE_WEBSERVER
+#define ENABLE_RTSPSERVER
+
+#ifdef ENABLE_OLED
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library, or in this case,
+// in the following lines
+
+// -----------------I2C-----------------
+// -- NEED TO DEFINE PINS IN ESP32CAM --
+// ----------- 3 NEW LINES -------------
+#define I2C_SDA 14 // SDA Connected to GPIO 14
+#define I2C_SCL 15 // SCL Connected to GPIO 15
+TwoWire I2Cbus = TwoWire(0);
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &I2Cbus, OLED_RESET); // &wire changed to &I2Cbus
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+#endif
+
+OV2640 cam;
+
+#ifdef ENABLE_WEBSERVER
+WebServer server(80);
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+WiFiServer rtspServer(8554);
+#endif
+
+#ifdef SOFTAP_MODE
+IPAddress apIP = IPAddress(192, 168, 1, 1);
+#else
+#include "wifikeys.h"
+#endif
+
+#ifdef ENABLE_WEBSERVER
+void handle_jpg_stream(void)
+{
+    WiFiClient client = server.client();
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+    server.sendContent(response);
+
+    while (1)
+    {
+        cam.run();
+        if (!client.connected())
+            break;
+        response = "--frame\r\n";
+        response += "Content-Type: image/jpeg\r\n\r\n";
+        server.sendContent(response);
+
+        client.write((char *)cam.getfb(), cam.getSize());
+        server.sendContent("\r\n");
+        if (!client.connected())
+            break;
+    }
+}
+
+void handle_jpg(void)
+{
+    WiFiClient client = server.client();
+
+    cam.run();
+    if (!client.connected())
+    {
+        return;
+    }
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-disposition: inline; filename=capture.jpg\r\n";
+    response += "Content-type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+    client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound()
+{
+    String message = "Server is running!\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    server.send(200, "text/plain", message);
+}
+#endif
+
+void printText(String msg) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(msg);
+
+  display.display();
+  //delay(250);
+}
+
+void lcdMessage(String msg)
+{
+  #ifdef ENABLE_OLED
+    //scrollText(msg);
+    printText(msg);
+  #endif
+}
+
+void setup()
+{
+  
+  // INIT SERIAL (debug purposes)
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    ;
+  }
+  Serial.println(F("Serial connected"));
+
+#ifdef ENABLE_OLED
+    // NEED TO INIT I2C before anything else (NEW LINE)
+    I2Cbus.begin(I2C_SDA, I2C_SCL, 100000);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+    }
+
+    // good news
+    Serial.println(F("SSD1306 allocation successful"));
+
+    // Show initial display buffer contents on the screen --
+    // the library initializes this with an Adafruit splash screen.
+    display.display();
+    delay(2000); // Pause for 2 seconds
+
+#endif
+
+#ifdef LED_BUILTIN
+    // led blink
+    pinMode(LED_BUILTIN, OUTPUT);
+#endif
+
+    lcdMessage("booting");
+
+    //cam.init(esp32cam_config);
+    cam.init(esp32cam_aithinker_config);
+
+    IPAddress ip;
+
+#ifdef SOFTAP_MODE
+    const char *hostname = "devcam";
+    // WiFi.hostname(hostname); // FIXME - find out why undefined
+    lcdMessage("starting softAP");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    bool result = WiFi.softAP(hostname, "12345678", 1, 0);
+    if (!result)
+    {
+        Serial.println("AP Config failed.");
+        return;
+    }
+    else
+    {
+        Serial.println("AP Config Success.");
+        Serial.print("AP MAC: ");
+        Serial.println(WiFi.softAPmacAddress());
+
+        ip = WiFi.softAPIP();
+    }
+#else
+    lcdMessage(String("join ") + ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(F("."));
+        display.print(".");
+        display.display();
+    }
+    ip = WiFi.localIP();
+    Serial.println(F("WiFi connected"));
+    Serial.println("");
+    Serial.println(ip);
+#endif
+
+    lcdMessage(ip.toString());
+
+#ifdef ENABLE_WEBSERVER
+    server.on("/", HTTP_GET, handle_jpg_stream);
+    server.on("/jpg", HTTP_GET, handle_jpg);
+    server.onNotFound(handleNotFound);
+    server.begin();
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+    rtspServer.begin();
+#endif
+}
+
+CStreamer *streamer;
+CRtspSession *session;
+WiFiClient client; // FIXME, support multiple clients
+
+void loop()
+{
+#ifdef ENABLE_WEBSERVER
+    server.handleClient();
+#endif
+
+
+#ifdef LED_BUILTIN
+    // led blink control
+    static uint32_t ledtimer = millis();
+    if(millis() > ledtimer) {
+    ledtimer = millis()+1000;
+        if(ledon) {
+            digitalWrite(LED_BUILTIN, LOW);
+            ledon = false;
+        } else {
+            digitalWrite(LED_BUILTIN, HIGH);
+            ledon = true;  
+        }
+    }
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+    uint32_t msecPerFrame = 100;
+    static uint32_t lastimage = millis();
+
+    // If we have an active client connection, just service that until gone
+    // (FIXME - support multiple simultaneous clients)
+    if(session) {
+        session->handleRequests(0); // we don't use a timeout here,
+        // instead we send only if we have new enough frames
+
+        uint32_t now = millis();
+        if(now > lastimage + msecPerFrame || now < lastimage) { // handle clock rollover
+            session->broadcastCurrentFrame(now);
+            lastimage = now;
+
+            // check if we are overrunning our max frame rate
+            now = millis();
+            if(now > lastimage + msecPerFrame)
+                printf("exceeding max frame rate of %d ms\n", now - lastimage);
+        }
+
+        if(session->m_stopped) {
+            delete session;
+            delete streamer;
+            session = NULL;
+            streamer = NULL;
+        }
+    }
+    else {
+        client = rtspServer.accept();
+
+        if(client) {
+            //streamer = new SimStreamer(&client, true);             // our streamer for UDP/TCP based RTP transport
+            streamer = new OV2640Streamer(&client, cam);             // our streamer for UDP/TCP based RTP transport
+
+            session = new CRtspSession(&client, streamer); // our threads RTSP session and state
+        }
+    }
+#endif
+}
